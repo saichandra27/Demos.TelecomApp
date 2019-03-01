3488e4362413f6c31cd97d619b5324c73eda0948
saichandra27 <48094296+saichandra27@users.noreply.github.com>
2019-03-01 15:51:21 +0800

    Add files via upload

---
+++ App_Code/TAS_Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for TAS_Helper
+/// </summary>
+public static class TAS_Helper
+{
+    public static string getConnectionString()
+    {
+        return ConfigurationManager.ConnectionStrings["TAS_connectionstring"].ConnectionString;
+    }
+}
